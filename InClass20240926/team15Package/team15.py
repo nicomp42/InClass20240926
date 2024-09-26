107432ef8c4565c04524f4afa99b34a390ccf12d
Ryan Dew <dewrm@mail.uc.edu>
2024-09-26 08:43:52 -0400

    Team15- Created a dictionary with nfl teams the city they are in.

---
+++ InClass20240926/team15Package/team15.py
@@ -0,0 +1,13 @@
+#team15.py
+
+
+def team15():
+
+    """
+    A funtion that returns a list of cities and their NFL team
+    @return dict: A list of dictionaries that has  a city and their corresponding NFl team.
+    """
+  
+    nflDict = [{'Cincinnati' : 'Bengals'}, {'Cleveland' : 'Browns'}, {'Pittsburgh' : 'Steelers'},  {'Baltimore' : 'Ravens'}, {'New England' : 'Patriots'}]
+    
+    return nflDict
