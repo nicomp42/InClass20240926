23fe378d0fd952ee344333f2d1e844468241417a
willgpa <wgpadgett@gmail.com>
2024-09-26 08:34:25 -0400

    Added team8Package and tean8 function

---
+++ InClass20240926/team8Package/Team8.py
@@ -0,0 +1,13 @@
+
+
+def team8():
+   """
+    A function that returns a list of dictionaries of student data
+    @return list: The list of combinded student dictionaries 
+    """ 
+   people = [{"name":"Jane Doe", "Major":"IS"},
+                 {"name":"John Jane", "Major": "IS"},
+                 {"name":"John Doe", "Major": "IS"}]
+   return people
+
+    
