d6bc202112ac35182457c4963888f8fd0ec3ffae
Ishani Roy Chowdhury <roychoii@mail.uc.edu>
2024-09-26 08:44:14 -0400

    Group3 Dictionary Added

--- InClass20240926/Group3/Group3.py
+++ InClass20240926/Group3/Group3.py
@@ -1,5 +1,23 @@
 # Group3.py
 
-# This is where our function will go 
-
-# Test
+def Group3():
+    '''
+    Function should return a list of dictionaries
+    @return dict: a list of dictionaries
+    '''
+    
+    teamMascotsOhio = [{"Ohio State University":"Buckeyes",
+                        "University of Cincinnati":"Bearcats",
+                        "University of Dayton":"Flyers"}]
+    
+    teamMascotsEast = [{"Boston University":"Bulldogs",
+                        "UNC":"Tar Heels",
+                        "NYU":"Bobcats"}]
+    
+    teamMascotsWest = [{"UCLA":"Bruins",
+                        "UCSD":"Triton",
+                        "Stanford":"Tree"}]
+    
+    teamMascotsUSA = [teamMascotsOhio, teamMascotsEast, teamMascotsWest]
+    
+    return teamMascotsUSA
