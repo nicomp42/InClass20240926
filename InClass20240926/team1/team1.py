1a858ede898fac05676b3d8d287f88e57aa54472
tipkemam <tipkemam@VDI-LCB-IC-020>
2024-09-26 08:52:50 -0400

    Team One (1) Changes

---
+++ InClass20240926/team1/team1.py
@@ -0,0 +1,18 @@
+
+#team1.py
+
+
+def team1():
+    
+        """
+        Function creating dictionaries of American Fast Food Data
+        @return list: List of Fast Food Dictionarieis 
+        """
+    
+        team1Data = [{"name:":"Mcdonalds","order:":"Big Mac","price:":"$7"}, 
+                    {"name:":"Burger King","order:":"Whopper","price:":"$8"},
+                    {"name:":"Wendy's","order:":"Daves Single","price:":"$6"},
+                    {"name:":"White Castle","order:":"Slider","price:":"$2"}]
+        return team1Data
+
+
