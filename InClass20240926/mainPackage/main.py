f5e8dfe734f1cd7c3f67472e3b1ecea135f09219
nicomp42 <nicholdw@ucmail.uc.edu>
2024-09-26 08:08:06 -0400

    Add project files.

---
+++ InClass20240926/mainPackage/main.py
@@ -0,0 +1,19 @@
+#main.py
+from nicholdwPackage.nicholdw import *
+# Do not edit this module at all.
+# Create some new artifacts in the project...
+# Name your package {team}Package
+# Name your module {team}.py
+# Name your function {team}
+# The {} are for clarity and are not part of any name
+# Your function should return a list of dictionaries, just a few rows.
+
+
+if __name__ == "__main__":
+    print("testing your code...")
+    print("nicholdw")
+    data = nicholdw()
+    for item in data:
+        print(item)
+    print("*****************")
+    
---
+++ InClass20240926/nicholdwPackage/nicholdw.py
@@ -0,0 +1,10 @@
+
+# nicholdw.py
+
+def nicholdw():
+    """
+    A function that returns a list of dictionaries
+    @return dict: A list of dictionaries
+    """
+    myData = [{"Some Key":"Some value"}]
+    return myData
