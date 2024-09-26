627e4a0015de6fb7a5af329fa0ed91d12a354b53
fridayaj <fridayaj@mail.uc.edu>
2024-09-26 08:34:09 -0400

    Added team10 list of dictionaries

---
+++ InClass20240926/team10Package/team10.py
@@ -0,0 +1,9 @@
+def team10():
+    """
+    A function that returns a list of dictionaries
+    @return dict: A list of dictionaries
+    """
+    team10Data = [{"Name":"Aidan", "Team":"Packers", "Division":"NFC North"},
+                   {"Name":"Roman", "Team":"Packers", "Division":"NFC North"},
+                   {"Name":"Kyle", "Team":"Bengals", "Division":"AFC North"}]
+    return team10Data
