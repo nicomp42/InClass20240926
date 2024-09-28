8e493d64bd7c88d1a127ca4de987459c5ada027c
nicomp42 <nicholdw@ucmail.uc.edu>
2024-09-28 16:36:28 -0400

    Fixed all the merge conflicts and added all the team functions to the main module.

--- InClass20240926/mainPackage/main.py
+++ InClass20240926/mainPackage/main.py
@@ -1,6 +1,21 @@
 #main.py 
 from nicholdwPackage.nicholdw import *
-from Group3.Group3 import *
+from team1.team1 import *                  # Nonconforming
+from team2Package.team2 import *           # Nonconforming
+from Group3.Group3 import *                # Nonconforming
+from team04Package.team04 import *         # Nonconforming
+from Team5Package.Team5 import *           # Nonconforming
+from team07Package.team07 import *         # Nonconforming
+from team8Package.Team8 import *           # Nonconforming
+from team10Package.team10 import *         # Nonconforming
+from team12Package.team12 import *         # Nonconforming
+from team13Package.team13 import *         # Nonconforming
+from Group14Package.Group14 import *         
+from team15Package.team15 import *         # Nonconforming
+from team17Package.team17 import *         # Nonconforming
+from Team19.Team19 import *                # Nonconforming
+from Team20package1.Team20 import *        # Nonconforming
+
 # Do not edit this module at all.
 # Create some new artifacts in the project...
 # Name your package {team}Package
@@ -16,4 +31,16 @@ if __name__ == "__main__":
     data = nicholdw()
     for item in data:
         print(item)
-    print("*****************")
+        
+    group_names = ["team1",   "team2",  "Group3", "team04",  "Team5", "group6",  "group7", "team8",   "group9",  "team10",
+                   "group11", "team12", "team13", "Group14", "team15", "group16", "team17", "group18", "Team19",  "Team20"]
+
+    for group_name in group_names:
+        print(group_name, " ****************************************************************************************************")
+        try:
+            function_name = group_name
+            items = eval(function_name)()
+            for item in items:
+                print(item)
+        except Exception as e:
+            print("Error in ", group_name, ":", e)
